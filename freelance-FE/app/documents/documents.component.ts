1cf684a7cf76f37d8c100062f7495475907f35f1
Preston Matheson <pmphoto123@gmail.com>
2017-04-18 14:20:43 -0600

    created the first connection between the angular app and the rails api

--- freelance-FE/app/documents/documents.component.ts
+++ freelance-FE/app/documents/documents.component.ts
@@ -1,36 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'documents',
+  moduleId:    module.id,
+  selector:    'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: [ 'documents.component.css'],
+  providers: [ DocumentService ],
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  documents: Document[] = [
-    {
-      title: 'My First Doc',
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '04/17/17',
-      image_url: 'https://static.pexels.com/photos/6415/apple-desk-office-working.jpg',
-    },
-    {
-      title: 'My Second Doc',
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '04/17/17',
-      image_url: 'https://static.pexels.com/photos/6415/apple-desk-office-working.jpg',
-    },
-    {
-      title: 'My Last Doc',
-      description: 'asdfasdfasdf asdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '04/17/17',
-      image_url: 'https://static.pexels.com/photos/6415/apple-desk-office-working.jpg',
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService;
+    ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments())
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+          );
+  }
 }
 
